2fcff181dac61d5d623847d54403649244c8053e
UrielSJP12 <143725665+UrielSJP12@users.noreply.github.com>
2023-11-06 07:28:00 -0600

    Add files via upload

---
+++ CorreccionUno.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package correccionuno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CorreccionUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    Scanner jp = new Scanner(System.in);
+         boolean seguirComprando = true;
+        while(seguirComprando){
+            System.out.println("Bienvenido a la Cafeteria. Seleccione tu Cafe:");
+            
+            System.out.println("1. Cappuccino");
+            System.out.println("2. Latte");
+            System.out.println("3. Matcha");
+            System.out.println("4. Salir");
+            
+            int opcion = jp.nextInt();
+            
+            if(opcion ==4){
+                seguirComprando = false;
+                
+                System.out.println("Gracias por su compra. Hata Pronto.");
+            }else if (opcion >=1 && opcion <= 3){
+                String[] sabores = {"Cappuccino", "Latte", "Matcha"};
+                double [][] precios = {{23.30, 30.0, 37.5}, {32.5, 38.4, 42.5}, {41.5, 48.9, 52.5}};
+                
+                System.out.println("Seleccione el tamaño (CH/M/G): ");
+                String tamaño = jp.next();
+                
+                System.out.println("¿Desea leche deslactosada? (Si/No): ");  
+                String leche = jp.next();
+                
+                int saborElegido = opcion - 1;
+                double precioCafe = precios[saborElegido][tamaño.equals("CH") ? 0 : (tamaño.equals("M") ? 1 : 2)];
+                
+                if(leche.equalsIgnoreCase("Si")){
+                    precioCafe =+ 12.0; 
+                }
+                System.out.println("Cafe comprado: " + sabores[saborElegido]);
+                System.out.println("Tipo de leche: " + 
+                        (leche.equalsIgnoreCase("Si") ? "Deslactosada" : "Normal"));
+                
+                System.out.println("El total de la cuenta es de: $" + precioCafe);
+            }else{
+                System.out.println("Opcion no valida. Seleccione otra opcion.");
+            
+            }
+            
+        }
+        jp.close();
+        
+        
+    }
+}
+
